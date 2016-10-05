c7d4793ef864784762b262e896eb8d80239a5f38
uplinkhacker1992@gmail.com <Jacker1992>
2016-10-06 00:08:06 +0700

    - Update all Signed-off-by: uplinkhacker1992@gmail.com <Jacker1992>

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# Zodiac-Sign
-[![Coverage Status](https://coveralls.io/repos/github/uplinkhacker1992/Zodiac-Sign/badge.svg?branch=master)](https://coveralls.io/github/uplinkhacker1992/Zodiac-Sign?branch=master)
+# Example module Zodiac-Sign
+
+=========
+
+[![Build Status](https://travis-ci.org/uplinkhacker1992/Zodiac-Sign.svg?branch=master)](https://travis-ci.org/uplinkhacker1992/Zodiac-Sign) [![Coverage Status](https://coveralls.io/repos/github/uplinkhacker1992/Zodiac-Sign/badge.svg?branch=master)](https://coveralls.io/github/uplinkhacker1992/Zodiac-Sign?branch=master)
+[![License](https://img.shields.io/github/license/uplinkhacker1992/zodiac-sign.svg)](https://github.com/uplinkhacker1992/Zodiac-Sign/blob/master/LICENSE)
+
+
+
+A small library providing utility methods `getZodiac` to check your Zodiac Sign from date of birth
+
+## Installation
+
+npm install
+
+## Tests
+
+npm test
+
+## Contributing
+
+ToanDTC
+
+## Release History
+
+# * 1.0.0 Initial release
+# * 1.1.0 Update new features
--- test/index.js
+++ test/index.js
@@ -1,42 +1,90 @@
-const Zodiac = require('../src/Zodiac')
-var assert = require('assert');
-describe('Zodiac', function() {
-  describe('#getZodiac()', function() {
-it('15/1/1990 - bạn thuộc cung Ma Kết', function() {
-                assert.equal('Ma Kết', Zodiac.getZodiac('15/1/1990'))
+var zodiac = require('../src/zodiac')
+var expect = require('chai').expect;
+describe('Zodiac Sign:', (done) => {
+    describe('getZodiac:', (done) => {
+        it('15/1/1990 - Ma Kết', (done) => {
+            zodiac.getZodiac('15/1/1990', (err, s) => {
+                expect(err).to.equal(null);
+                expect(s).to.equal('Ma Kết');
+                done();
+            });
+        });
+        it('15/2/1990 - Bảo Bình', (done) => {
+            zodiac.getZodiac('15/2/1990', (err, s) => {
+                expect(err).to.equal(null);
+                expect(s).to.equal('Bảo Bình');
+                done();
+            });
+        });
+        it('15/3/1991 - Song Ngư', (done) => {
+            zodiac.getZodiac('15/3/1991', (err, s) => {
+                expect(err).to.equal(null);
+                expect(s).to.equal('Song Ngư');
+                done();
+            });
+        });
+        it('15/4/1991 - Bạch Dương', (done) => {
+            zodiac.getZodiac('15/4/1991', (err, s) => {
+                expect(err).to.equal(null);
+                expect(s).to.equal('Bạch Dương');
+                done();
+            });
+        });
+        it('15/5/1992 - Kim Ngưu', (done) => {
+            zodiac.getZodiac('15/5/1992', (err, s) => {
+                expect(err).to.equal(null);
+                expect(s).to.equal('Kim Ngưu');
+                done();
+            });
+        });
+        it('15/6/1992 - Song Tử', (done) => {
+            zodiac.getZodiac('15/6/1992', (err, s) => {
+                expect(err).to.equal(null);
+                expect(s).to.equal('Song Tử');
+                done();
+            });
+        });
+        it('15/7/1993 - Cự Giải', (done) => {
+            zodiac.getZodiac('15/7/1993', (err, s) => {
+                expect(err).to.equal(null);
+                expect(s).to.equal('Cự Giải');
+                done();
+            });
+        });
+        it('15/8/1993 - Sử Tử', (done) => {
+            zodiac.getZodiac('15/8/1993', (err, s) => {
+                expect(err).to.equal(null);
+                expect(s).to.equal('Sử Tử');
+                done();
+            });
+        });
+        it('15/9/1994 - Xử Nữ', (done) => {
+            zodiac.getZodiac('15/9/1994', (err, s) => {
+                expect(err).to.equal(null);
+                expect(s).to.equal('Xử Nữ');
+                done();
+            });
+        });
+        it('15/10/1994 - Thiên Bình', (done) => {
+            zodiac.getZodiac('15/10/1994', (err, s) => {
+                expect(err).to.equal(null);
+                expect(s).to.equal('Thiên Bình');
+                done();
+            });
+        });
+        it('15/11/1995 - Thần Nông', (done) => {
+            zodiac.getZodiac('15/11/1995', (err, s) => {
+                expect(err).to.equal(null);
+                expect(s).to.equal('Thần Nông');
+                done();
+            });
+        });
+        it('15/12/1996 - Nhân Mã', (done) => {
+            zodiac.getZodiac('15/12/1996', (err, s) => {
+                expect(err).to.equal(null);
+                expect(s).to.equal('Nhân Mã');
+                done();
+            });
+        });
     });
-it('15/2/1990 - bạn thuộc cung Bảo Bình', function() {
-                assert.equal('Bảo Bình', Zodiac.getZodiac('15/2/1990'))
-    });
-it('15/3/1991 - bạn thuộc cung Song Ngư', function() {
-                assert.equal('Song Ngư', Zodiac.getZodiac('15/3/1991'))
-    });
-it('15/4/1991 - bạn thuộc cung Bạch Dương', function() {
-                assert.equal('Bạch Dương', Zodiac.getZodiac('15/4/1991'))
-    });
-it('15/5/1992 - bạn thuộc cung Kim Ngưu', function() {
-                assert.equal('Kim Ngưu', Zodiac.getZodiac('15/5/1992'))
-    });
-it('15/6/1992 - bạn thuộc cung Song Tử', function() {
-                assert.equal('Song Tử', Zodiac.getZodiac('15/6/1992'))
-    });
-it('15/7/1993 - bạn thuộc cung Cự Giải', function() {
-                assert.equal('Cự Giải', Zodiac.getZodiac('15/7/1993'))
-    });
-it('15/8/1993 - bạn thuộc cung Sử Tử', function() {
-                assert.equal('Sử Tử', Zodiac.getZodiac('15/8/1993'))
-    });
-it('15/9/1994 - bạn thuộc cung Xử Nữ', function() {
-                assert.equal('Xử Nữ', Zodiac.getZodiac('15/9/1994'))
-    });
-it('15/10/1994 - bạn thuộc cung Thiên Bình', function() {
-                assert.equal('Thiên Bình', Zodiac.getZodiac('15/10/1994'))
-    });
-it('15/11/1995 - bạn thuộc cung Thần Nông', function() {
-                assert.equal('Thần Nông', Zodiac.getZodiac('15/11/1995'))
-    });
-it('15/12/1996 - bạn thuộc cung Nhân Mã', function() {
-                assert.equal('Nhân Mã', Zodiac.getZodiac('15/12/1996'))
-    });
-  });
 });
